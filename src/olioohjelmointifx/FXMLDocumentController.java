be28dd2cf87df59696d99e5f6818bde7e0a241d4
tommi <tommi@tommis-mbp>
2016-11-26 19:53:10 +0200

    Week 10 Assignment 1

--- src/olioohjelmointifx/FXMLDocumentController.java
+++ src/olioohjelmointifx/FXMLDocumentController.java
@@ -5,22 +5,11 @@
  */
 package olioohjelmointifx;
 
-import OlioOhjelmointi.*;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
+
+
 
 /**
  *
@@ -28,60 +17,15 @@ import javafx.scene.input.MouseEvent;
  */
 public class FXMLDocumentController implements Initializable {
 
-    @FXML
-    private Button buyButton;
-    @FXML
-    private Button moneyButton;
-    @FXML
-    private Button returnButton;
-    @FXML
-    private ComboBox<Bottle> bottleDisplay;
-    @FXML
-    private TextArea outputField;
-    @FXML
-    private Slider moneySlider;
-        
-    BottleDispenser d;
-    ReadAndWriteIO io;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        d = BottleDispenser.getInstance();
-        io = new ReadAndWriteIO ();
+        
         updateCombo();
-    }
-    @FXML
-    private void buyBottle(ActionEvent event) {
-        outputField.setText(d.buyBottle(bottleDisplay.getValue())+outputField.getText());
-        updateCombo();
-    }
-
-    @FXML
-    private void insertMoney(ActionEvent event) {
-        outputField.setText(d.addMoney(moneySlider.getValue())+outputField.getText());
-        moneySlider.setValue(1);
-        moneyButton.setText("Insert\n" + String.format("%.2f", moneySlider.getValue()));
-    }
-
-    @FXML
-    private void returnMoney(ActionEvent event) {
-        outputField.setText(d.returnMoney()+outputField.getText());
-    }
-
-    @FXML
-    private void updateSlider(MouseEvent event) {
-        moneyButton.setText("Insert\n" + String.format("%.2f", moneySlider.getValue()));
     }
     
-    private void updateCombo()
+    public void updateCombo ()
     {
-        bottleDisplay.getItems().removeAll(bottleDisplay.getItems());
-        bottleDisplay.getItems().addAll(d.getBottleArray());
-    }
-
-    @FXML
-    private void printReceipt(ActionEvent event) {
-        io.writeFile(d.getReceipt(), "output.txt");
-        outputField.setText("Kuitti tulostettu!\n"+outputField.getText());
+        
     }
 }
