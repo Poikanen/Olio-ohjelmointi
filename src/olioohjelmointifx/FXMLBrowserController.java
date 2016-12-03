c5728f6b667b19b33fb101b05e0a1e29a9b0508c
tommi <tommi@tommis-mbp>
2016-12-04 00:48:26 +0200

    Week 11 Assignment 4

--- src/olioohjelmointifx/FXMLBrowserController.java
+++ src/olioohjelmointifx/FXMLBrowserController.java
@@ -24,13 +24,16 @@ public class FXMLBrowserController implements Initializable {
     private TextField addressField;
     @FXML
     private WebView web;
-
+    
+    private String previousPage;
+    private String currentPage;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        previousPage ="";
+        currentPage = "";
     }    
 
     @FXML
@@ -38,8 +41,15 @@ public class FXMLBrowserController implements Initializable {
         if (addressField.getText().equals("index.html"))
         {
             web.getEngine().load(getClass().getResource("index.html").toExternalForm());
+            currentPage = web.getEngine().getLocation();
         }
         else web.getEngine().load("http://"+addressField.getText());
+        
+        if(!web.getEngine().getLocation().equals(currentPage))
+        {
+        previousPage = currentPage;
+        }
+        currentPage = web.getEngine().getLocation();
     }
 
     @FXML
@@ -56,6 +66,16 @@ public class FXMLBrowserController implements Initializable {
     private void restoreAction(ActionEvent event) {
         web.getEngine().executeScript("initialize()");
     }
-    
+
+    @FXML
+    private void previousAction(ActionEvent event) {
+            web.getEngine().load(previousPage);
+        
+    }
+
+    @FXML
+    private void nextAction(ActionEvent event) {
+        web.getEngine().load(currentPage);
+    }
     
 }
