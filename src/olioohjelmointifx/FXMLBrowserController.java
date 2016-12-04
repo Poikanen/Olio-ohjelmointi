deb2e1bd8c896198e09d005ba2597b5b1d5ac90d
tommi <tommi@tommis-mbp>
2016-12-04 02:24:15 +0200

    Week 11 Assignment 5

--- src/olioohjelmointifx/FXMLBrowserController.java
+++ src/olioohjelmointifx/FXMLBrowserController.java
@@ -6,6 +6,11 @@
 package olioohjelmointifx;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -25,15 +30,19 @@ public class FXMLBrowserController implements Initializable {
     @FXML
     private WebView web;
     
-    private String previousPage;
-    private String currentPage;
+    List<String> history;
+    ListIterator itr;
+    ListIterator updater;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        previousPage ="";
-        currentPage = "";
+        history = new ArrayList<>();
+        Collections.addAll(history, "","","","","","","","","","","");
+        System.out.println(history);
+        itr = history.listIterator(history.size());
+        
     }    
 
     @FXML
@@ -41,15 +50,14 @@ public class FXMLBrowserController implements Initializable {
         if (addressField.getText().equals("index.html"))
         {
             web.getEngine().load(getClass().getResource("index.html").toExternalForm());
-            currentPage = web.getEngine().getLocation();
         }
         else web.getEngine().load("http://"+addressField.getText());
         
-        if(!web.getEngine().getLocation().equals(currentPage))
+        itr = history.listIterator(history.size());
+        if(!web.getEngine().getLocation().equals(itr.previous()))
         {
-        previousPage = currentPage;
+            updateHistory();
         }
-        currentPage = web.getEngine().getLocation();
     }
 
     @FXML
@@ -69,13 +77,49 @@ public class FXMLBrowserController implements Initializable {
 
     @FXML
     private void previousAction(ActionEvent event) {
-            web.getEngine().load(previousPage);
-        
+        if (itr.hasPrevious())
+        {
+            String temp = (String)itr.previous();
+            if (!temp.isEmpty())
+            {
+                if (web.getEngine().getLocation().equals(temp))
+                {
+                    temp = (String)itr.previous();
+                }
+                web.getEngine().load((String)temp);
+            }
+            else 
+            {
+                temp = (String)itr.next();
+            }
+        }
     }
 
     @FXML
     private void nextAction(ActionEvent event) {
-        web.getEngine().load(currentPage);
+        if (itr.hasNext())
+        {
+            String temp = (String)itr.next();
+            while (web.getEngine().getLocation().equals(temp))
+            {
+                temp = (String)itr.next();
+            }
+            web.getEngine().load((String)temp);
+        }
     }
     
+    private void updateHistory()
+    {
+        updater = history.listIterator(history.size());
+        Object temp1 = updater.previous();
+        Object temp2;
+        while (updater.hasPrevious())
+        {
+            temp2= updater.previous();
+            updater.set(temp1);
+            temp1 = temp2;
+        }
+        itr.set(web.getEngine().getLocation());
+        System.out.println(history);
+    }
 }
